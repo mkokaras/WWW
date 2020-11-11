ab3079b78dedfc85e113695084c79ef517ba63d0
mkokaras <72735503+mkokaras@users.noreply.github.com>
2020-11-11 17:24:40 +0200

    Delete product_db.sql

--- 3.JPA Hibernate/ProductJpa/product_db.sql
+++
@@ -1,8 +0,0 @@
-create database productdb;
-use productdb;
-create table products(
-barcode varchar(100) not null,
-color varchar(100) not null,
-name varchar(100) not null,
-description varchar(1000) not null,
-primary key(barcode));  
